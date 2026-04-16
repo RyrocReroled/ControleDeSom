4858bf9aad3d633d44b64158c4f57cc30de24ecf
felipe <felipecoelhao@gmail.com>
2026-04-16 20:12:40 -0300

    Inclusão do git pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/ControleDeSom/'
-    : '/'
+    : '/',
+
+    // Opcional: remove source maps para produção (deixa o build mais leve)
+  productionSourceMap: false,
+  
+  // Opcional: se tiver problemas com arquivos estáticos
+  assetsDir: 'static'
 }
+
